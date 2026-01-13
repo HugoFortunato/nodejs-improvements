ddeb9625e1794045b218da06143b54b1b5aa3c66
Hugo  Fortunato <hp.fortunato@gmail.com>
2026-01-13 15:25:37 -0300

    refactor: auth controller

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository';
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository';
 import { PrismaService } from '../prisma/prisma.service';
 import { PrismaQuestionsRepository } from '../prisma/repositories/prisma-questions-repository';
+import { PrismaStudentsRepository } from '../prisma/repositories/prisma-students-repository';
 import { PrismaQuestionCommentsRepository } from '../prisma/repositories/prisma-question-comments-repository';
 import { PrismaQuestionAttachmentsRepository } from '../prisma/repositories/prisma-question-attachments-repository';
 import { PrismaAnswersRepository } from '../prisma/repositories/prisma-answers-repository';
@@ -16,6 +17,10 @@ import { PrismaAnswerAttachmentsRepository } from '../prisma/repositories/prisma
       provide: QuestionsRepository,
       useClass: PrismaQuestionsRepository,
     },
+    {
+      provide: StudentsRepository,
+      useClass: PrismaStudentsRepository,
+    },
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
     PrismaAnswersRepository,
@@ -25,6 +30,7 @@ import { PrismaAnswerAttachmentsRepository } from '../prisma/repositories/prisma
   exports: [
     PrismaService,
     QuestionsRepository,
+    StudentsRepository,
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
     PrismaAnswersRepository,
